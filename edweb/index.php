5d2d8db2689b506f2d5fa2ac2d62afc1b38e4801
rageded <edwinportillo62@yahoo.com>
2015-09-24 20:35:47 -0700

    secure

--- edweb/index.php
+++ edweb/index.php
@@ -274,8 +274,8 @@
 
          $servername = "localhost";
          $username = "root";
-         $password = "EMT3life.";
-         $dbname = "edwindb";
+         $password = "Password";
+         $dbname = "database";
          $conn = mysqli_connect($servername, $username, $password, $dbname);
          if(!$conn){
            die("Connection failed: ".mysqli_connect_error());
